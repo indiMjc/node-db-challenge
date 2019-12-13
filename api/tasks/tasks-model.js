aee9a49e4cb4ce7e5aed8ee61de7a86ae5c19136
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-13 12:49:19 -0500

    get tasks with projectNamd and projectDescription works, mvp met

--- api/tasks/tasks-model.js
+++ api/tasks/tasks-model.js
@@ -1,8 +1,20 @@
 const db = require('../../data/dbConfig');
 
 module.exports = {
+  getTasks,
   addTask
 };
+
+function getTasks() {
+  return db
+    .select(
+      'tasks.description AS Task',
+      'projects.name AS ProjectName',
+      'projects.description AS ProjectDescription'
+    )
+    .from('tasks')
+    .join('projects', 'tasks.project_id', 'projects.id');
+}
 
 function addTask(task, id) {
   const taskData = {
--- api/tasks/tasks-router.js
+++ api/tasks/tasks-router.js
@@ -4,6 +4,17 @@ const Tasks = require('./tasks-model');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  Tasks.getTasks()
+    .then(tasks => {
+      res.status(201).json(tasks);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Could not get tasks.' });
+    });
+});
+
 router.post('/:id', (req, res) => {
   const newTask = req.body;
   const { id } = req.params;
